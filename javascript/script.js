4e85822fae9e846a7e4e178d0c39dda75a1f2f92
svalvastic <assogames2@gmail.com>
2022-09-02 15:35:55 +0200

    ✨ feat(script): add script

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,52 @@
+const button = document.getElementById("generateGrid");
+const grid = document.querySelector(".grid");
+
+const getRandomNumbers = (quantity) => {
+    const array = [];
+    while (array.length < quantity) {
+        const number = Math.floor(Math.random() * quantity) + 1;
+        let check = false
+        array.forEach((value) => {
+            if (value === number)
+                check = true;
+        })
+
+        if (!check) {
+            array.push(number)
+        }
+
+    }
+    return array;
+}
 
 
-const button = document.getElementById('generateGrid');
+
+
+
+function createGrid() {
+    const randomNumbers = getRandomNumbers(100)
+  this.setAttribute("disabled", "");
+  console.log("entrato");
+  for (let i = 1; i <= 100; i++) {
+    const cell = createCell(i);
+    grid.append(cell);
+  }
+}
+
+function clickedColor() {
+   this.classList.add((parseInt(this.innerText) % 2 === 0)? "pari" : "dispari")
+ }
+
+
+const createCell = (cellContent) => {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    cell.append(cellContent)
+    cell.addEventListener("click", clickedColor)
+  return cell;
+};
+
+
+
+
+button.addEventListener("click", createGrid);
