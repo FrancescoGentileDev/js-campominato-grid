207c7cc402a6b08340785c0e606509b19093afbf
svalvastic <assogames2@gmail.com>
2022-09-02 15:47:36 +0200

    🌈 style(header): add header

--- javascript/script.js
+++ javascript/script.js
@@ -1,33 +1,46 @@
 const button = document.getElementById("generateGrid");
 const grid = document.querySelector(".grid");
-
+const gridSize = 100
+/**
+ * 
+ * @param {*} quantity max dei numeri generati
+ * @returns ritorna un array della grandezza richiesta con numeri diversi tra loro
+ */
 const getRandomNumbers = (quantity) => {
-    const array = [];
-    while (array.length < quantity) {
-        const number = Math.floor(Math.random() * quantity) + 1;
-        let check = false
-        array.forEach((value) => {
-            if (value === number)
-                check = true;
-        })
+  let array = [];
+  // create an array and fill with numbers
+  for (let i = 1; i <= quantity; i++) {
+    array.push(i);
+  }
+  //RANDOMIZE ARRAY
+  for (let i = array.length - 1; i >= 0; i--) {
+    //Pesco un numero a caso che mi servirà a sorteggiare l'indice da scambiare
+    let randomNumber = Math.floor(Math.random() * (i + 1));
 
-        if (!check) {
-            array.push(number)
-        }
+    //salvo il valore dell'array sull'indice di i corrente
+    let temp = array[i];
 
-    }
-    return array;
+    //Scambio i valori degli indici tra di loro
+    array[i] = array[randomNumber];
+    array[randomNumber] = temp;
+  }
+  return array;
 }
 
-
-
+const createCell = (cellContent) => {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    cell.append(cellContent)
+    cell.addEventListener("click", clickedColor)
+  return cell;
+};
 
 
 function createGrid() {
-    const randomNumbers = getRandomNumbers(100)
+    const randomNumbers = getRandomNumbers(gridSize)
   this.setAttribute("disabled", "");
   console.log("entrato");
-  for (let i = 1; i <= 100; i++) {
+  for (let i = 1; i <= gridSize; i++) {
     const cell = createCell(randomNumbers[i-1]);
     grid.append(cell);
   }
@@ -39,13 +52,6 @@ function clickedColor() {
  }
 
 
-const createCell = (cellContent) => {
-    const cell = document.createElement("div");
-    cell.classList.add("cell");
-    cell.append(cellContent)
-    cell.addEventListener("click", clickedColor)
-  return cell;
-};
 
 
 
